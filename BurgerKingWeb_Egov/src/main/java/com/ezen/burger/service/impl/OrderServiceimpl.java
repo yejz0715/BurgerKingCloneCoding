3e9ab3178a6abc61ea4cd45be875f4b0f368b027
JAVA02 <JAVA02@DESKTOP-L7MEE4F>
2022-02-11 16:46:24 +0900

    GUESTOrderList implement

--- BurgerKingWeb_Egov/src/main/java/com/ezen/burger/service/impl/OrderServiceimpl.java
+++ BurgerKingWeb_Egov/src/main/java/com/ezen/burger/service/impl/OrderServiceimpl.java
@@ -20,29 +20,6 @@ public class OrderServiceimpl extends EgovAbstractServiceImpl implements OrderSe
 	@Resource(name="CartDAO")
 	CartDAO cdao;
 	/*
-	public void insertOrderByGuest(ArrayList<CartVO> list, String id) {
-		// orders에 주문번호 하나 추가
-		odao.insertOrder(id);
-		
-		// 방금 추가한 주문의 oseq값을 추출(해당 아이디의 oseq값들중 indate기준으로 내림차순 정렬해
-		// 그 중 젤 위의 하나의 oseq 값을 추출한다.)
-		int oseq = odao.selectOseq(id);
-		
-		for(int i = 0; i < list.size(); i++) {
-			// 추출해낸 oseq 값과 cart들의 정보를 가지고 order_Detail의 내용을 추가한다.
-			odao.insertOrderDetail(list.get(i), oseq);
-			
-			// 해당 oseq 값을 가진 주문상세중 odseq 기준으로 내림차순 정렬해 가장 위의 odseq값을 추출한다.
-			int odseq = odao.selectOdseq(oseq);
-			
-			// 해당 카트의 cseq값을 가진 추가 재료의 oseq와 odseq에
-			// 추출한 두 값을 삽입해준다.
-			odao.insertseq(list.get(i).getCseq(), oseq, odseq);
-		}
-	}
-	public ArrayList<orderVO> getOrderByOseq(int oseq) {
-		return odao.getOrderByOseq(oseq);
-	}
 
 	public int getOseq(String odseq) {
 		return odao.getOseq(odseq);
@@ -118,5 +95,10 @@ public class OrderServiceimpl extends EgovAbstractServiceImpl implements OrderSe
 	@Override
 	public void deleteOrder2(HashMap<String, Object> paramMap) {
 		odao.b_deleteOrder2(paramMap);
+	}
+
+	@Override
+	public void getOrderByOseq(HashMap<String, Object> paramMap) {
+		odao.b_getOrderByOseq(paramMap);
 	}
 }
